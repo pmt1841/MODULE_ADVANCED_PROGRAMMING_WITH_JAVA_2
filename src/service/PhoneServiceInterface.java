77d046598cc7d8eff9884a9ead2449fa9bcf1a04
pmt1841 <phamminhtien1841@gmail.com>
2025-12-16 17:17:56 +0700

    Update PhoneServiceInterface.java

--- src/service/PhoneServiceInterface.java
+++ src/service/PhoneServiceInterface.java
@@ -9,9 +9,9 @@ public interface PhoneServiceInterface {
 
     HashMap<Integer,Phone> listPhone();
 
-    HashMap<Integer,Phone> searchPhoneByName(String name);
+    HashMap<Integer,Phone> searchPhone(String name);
 
-    Phone searchPhoneByID(int id);
+    Phone searchPhone(int id);
 
     void removePhone(int id);
 }
