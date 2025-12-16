c800e1fc2e8be0e0f7d499b17b0c0853f286bf57
pmt1841 <phamminhtien1841@gmail.com>
2025-12-16 17:56:46 +0700

    Update PhoneService.java

--- src/service/PhoneService.java
+++ src/service/PhoneService.java
@@ -9,22 +9,25 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class PhoneService implements PhoneServiceInterface {
+    private final String IMPORT_FILE_PATH = "src/data/importedPhones.csv";
+    private final String GENUINE_FILE_PATH = "src/data/genuinePhones.csv";
+
     private final FileInterface fileService = new FileService();
 
     @Override
     public void addPhone(HashMap<Integer, Phone> phoneList, boolean isImported) {
         if (isImported) {
-            fileService.writeToFile(phoneList, "data/importedPhone.csv");
+            fileService.writeToFile(phoneList, IMPORT_FILE_PATH);
         } else {
-            fileService.writeToFile(phoneList, "data/genuinePhone.csv");
+            fileService.writeToFile(phoneList, GENUINE_FILE_PATH);
         }
     }
 
     @Override
     public HashMap<Integer, Phone> listPhone() {
         HashMap<Integer, Phone> phoneList = new HashMap<>();
-        phoneList.putAll(fileService.readFromFile("data/genuinePhone.csv", false));
-        phoneList.putAll(fileService.readFromFile("data/importedPhone.csv", true));
+        phoneList.putAll(fileService.readFromFile(GENUINE_FILE_PATH, false));
+        phoneList.putAll(fileService.readFromFile(IMPORT_FILE_PATH, true));
         return phoneList;
     }
 
@@ -44,8 +47,22 @@ public class PhoneService implements PhoneServiceInterface {
     }
 
     @Override
-    public void removePhone(int id) {
-        HashMap<Integer, Phone> phoneList = listPhone();
-        phoneList.remove(id);
+    public void removePhone(Phone phone) {
+        HashMap<Integer, Phone> genuinePhoneList = fileService.readFromFile(GENUINE_FILE_PATH, false);
+        HashMap<Integer, Phone> importedPhoneList = fileService.readFromFile(IMPORT_FILE_PATH, true);
+        for (HashMap.Entry<Integer, Phone> entry : genuinePhoneList.entrySet()) {
+            if (entry.getValue().equals(phone)) {
+                genuinePhoneList.remove(entry.getKey());
+                break;
+            }
+            fileService.writeToFile(genuinePhoneList, GENUINE_FILE_PATH);
+        }
+        for (HashMap.Entry<Integer, Phone> entry : importedPhoneList.entrySet()) {
+            if (entry.getValue().equals(phone)) {
+                importedPhoneList.remove(entry.getKey());
+                break;
+            }
+            fileService.writeToFile(importedPhoneList, IMPORT_FILE_PATH);
+        }
     }
 }
